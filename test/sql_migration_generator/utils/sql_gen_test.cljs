0a4f90b125abe512261f73a8258ccd91ba99ffe1
Alex Afshar <contact@alexafshar.com>
2021-08-23 09:13:49 -0400

    Setup karma.
    Setup karma testing and made a note in README.org.

--- test/sql_migration_generator/utils/sql_gen_test.cljs
+++ test/sql_migration_generator/utils/sql_gen_test.cljs
@@ -2,3 +2,30 @@
   (:require [sql-migration-generator.utils.sql-gen :as sut]
             [cljs.test :as t :include-macros true]))
 
+
+;; Define test for this structure:
+;; {:tables [{:name "parent1"
+;;            :fields [:id "Long" :name "String"]}
+;;           {:name "child1"
+;;            :fields [:id "Long" :name "String"]}
+;;           {:name "child2"
+;;            :fields [:id "Long" :description "Text"]}
+;;           {:name "parent2"
+;;            :fields [:id "Long" :body "Text"]}
+;;           ]
+;;  :join-tables [{"parent2_child2"
+;;                 :fields [:parent2_id "Long"
+;;                          :child2_id "Long"]}]
+;;  :relationships [{"parent1"
+;;                   :has-many ["child1"]
+;;                   :has-one ["child2"]}
+;;                  {"parent2"
+;;                   :has-one ["child1"]}
+;;                  ]}
+;; to generate migrations.
+
+(t/deftest sql-structure
+  (t/testing "tables without relationships"
+    (t/is (= 1 1))
+    )
+  )
