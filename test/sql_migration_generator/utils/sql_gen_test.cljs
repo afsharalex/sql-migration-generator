06ffe3a07f007bbeda544d86fe5e6283c74f2376
Alex Afshar <contact@alexafshar.com>
2021-08-24 00:44:44 -0400

    More tests for tables with many fields.

--- test/sql_migration_generator/utils/sql_gen_test.cljs
+++ test/sql_migration_generator/utils/sql_gen_test.cljs
@@ -2,29 +2,8 @@
   (:require [sql-migration-generator.utils.sql-gen :as sut]
             [cljs.test :as t :include-macros true]))
 
-
-;; Define test for this structure:
-;; {:tables [{:name "parent1"
-;;            :fields [:id "Long" :name "String"]}
-;;           {:name "child1"
-;;            :fields [:id "Long" :name "String"]}
-;;           {:name "child2"
-;;            :fields [:id "Long" :description "Text"]}
-;;           {:name "parent2"
-;;            :fields [:id "Long" :body "Text"]}
-;;           ]
-;;  :join-tables [{"parent2_child2"
-;;                 :fields [:parent2_id "Long"
-;;                          :child2_id "Long"]}]
-;;  :relationships [{"parent1"
-;;                   :has-many ["child1"]
-;;                   :has-one ["child2"]}
-;;                  {"parent2"
-;;                   :has-one ["child1"]}
-;;                  ]}
-;; to generate migrations.
-
 (t/deftest sql-structure
+
   (t/testing "table without relationships"
     (t/is (= "CREATE TABLE TestTable ( ID int , Name varchar(255) );\n"
              (sut/gen-migration {:tables [{:name "TestTable"
@@ -34,6 +13,17 @@
 
     (t/is (= "CREATE TABLE TestTable ( ID int , Description TEXT );\n"
              (sut/gen-migration {:tables [{:name "TestTable"
+                                           :fields [{:name "ID" :type "int"}
+                                                    {:name "Description" :type "TEXT"}]}]
+                                 :relationships []})))
+    )
+
+  (t/testing "multiple tables without relationships"
+    (t/is (= "CREATE TABLE Table1 ( ID int , Name varchar(255) );\nCREATE TABLE Table2 ( ID int , Description TEXT );\n"
+             (sut/gen-migration {:tables [{:name "Table1"
+                                           :fields [{:name "ID" :type "int"}
+                                                    {:name "Name" :type "varchar(255)"}]}
+                                          {:name "Table2"
                                            :fields [{:name "ID" :type "int"}
                                                     {:name "Description" :type "TEXT"}]}]
                                  :relationships []})))
@@ -62,14 +52,8 @@
                                                     {:name "Description" :type "TEXT"}]}]
                                  :relationships []}))))
 
-  (t/testing "multiple tables without relationships"
-    (t/is (= "CREATE TABLE Table1 ( ID int , Name varchar(255) );\nCREATE TABLE Table2 ( ID int , Description TEXT );\n"
-             (sut/gen-migration {:tables [{:name "Table1"
-                                           :fields [{:name "ID" :type "int"}
-                                                    {:name "Name" :type "varchar(255)"}]}
-                                          {:name "Table2"
-                                           :fields [{:name "ID" :type "int"}
-                                                    {:name "Description" :type "TEXT"}]}]
-                                 :relationships []})))
+  (t/testing "multiple tables with many fields"
+    (t/is (= "CREATE TABLE TestTable (\n\tID int,\n\tName varchar(255),\n\tDescription TEXT,\n\tThings int\n);\nCREATE TABLE TestTable2 (\n\tID int,\n\tDescription TEXT,\n\tName varchar(255)\n);\n"))
     )
+
   )
