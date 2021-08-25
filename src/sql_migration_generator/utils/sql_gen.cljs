385e0902f4ad00f7628829a75a7ea4c1b7998fa0
Alex Afshar <contact@alexafshar.com>
2021-08-25 00:15:46 -0400

    Thinking about table relationships.

--- src/sql_migration_generator/utils/sql_gen.cljs
+++ src/sql_migration_generator/utils/sql_gen.cljs
@@ -1,6 +1,23 @@
 (ns sql-migration-generator.utils.sql-gen
   (:require [clojure.string :as str]))
 
+
+;; CREATE TABLE customers (
+;;     customer_id INT AUTO_INCREMENT PRIMARY KEY,
+;;     customer_name VARCHAR(100)
+;; );
+
+;; Want to specify the type as foreign key, in some optional data.
+;; Want to avoid specifying the field as well as the foreign key.
+;; CREATE TABLE orders (
+;;     order_id INT AUTO_INCREMENT PRIMARY KEY,
+;;     customer_id INT,
+;;     amount DOUBLE,
+;;     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+;; );
+;; {:foreign_key "customer_id" :references {:table "customers" :column "customer_id"}}
+;; A bit verbose.
+
 (defn gen-table-migration
   "Generate a single table migration statement."
   [table]
